5aa57f2beb36e7860bc2798234a7054ae6df594c
Felix Gudéhn <Felix@192-168-1-6.tpgi.com.au>
2016-10-30 02:04:59 +1100

    Created a custom widget for multiselection spinners and fixed the design of the filter activity

--- app/src/main/java/com/mad/cipelist/swiper/SearchFilterActivity.java
+++ app/src/main/java/com/mad/cipelist/swiper/SearchFilterActivity.java
@@ -5,15 +5,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.SeekBar;
 
 import com.mad.cipelist.R;
 import com.mad.cipelist.common.BaseActivity;
+import com.mad.cipelist.widgets.MultiSelectionSpinner;
 
 /**
  * Created by Felix on 29/10/2016.
  */
 
 public class SearchFilterActivity extends BaseActivity {
+
+    private MultiSelectionSpinner mDietSpinner;
+    private MultiSelectionSpinner mCuisineSpinner;
+    private MultiSelectionSpinner mAllergiesSpinner;
+    private MultiSelectionSpinner mCourseSpinner;
+    private SeekBar mCookingTimeSb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +31,31 @@ public class SearchFilterActivity extends BaseActivity {
         FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame);
         getLayoutInflater().inflate(R.layout.content_search_filter, contentFrameLayout);
 
+        mDietSpinner = (MultiSelectionSpinner) findViewById(R.id.diet_spinner);
+        mDietSpinner.setItems(getResources().getStringArray(R.array.diet_items));
+        mDietSpinner.setDefaultText("None Selected");
+
+        mCuisineSpinner = (MultiSelectionSpinner) findViewById(R.id.cuisine_spinner);
+        mCuisineSpinner.setItems(getResources().getStringArray(R.array.cuisine_items));
+        mCuisineSpinner.setDefaultText("None Selected");
+
+        mAllergiesSpinner = (MultiSelectionSpinner) findViewById(R.id.allergies_spinner);
+        mAllergiesSpinner.setItems(getResources().getStringArray(R.array.allergy_items));
+        mAllergiesSpinner.setDefaultText("None Selected");
+
+        mCourseSpinner = (MultiSelectionSpinner) findViewById(R.id.course_spinner);
+        mCourseSpinner.setItems(getResources().getStringArray(R.array.course_items));
+        mCourseSpinner.setDefaultText("None Selected");
+
+        mCookingTimeSb = (SeekBar) findViewById(R.id.cooking_time_bar);
+
         Button mStartSearchBtn = (Button) findViewById(R.id.start_search_btn);
         mStartSearchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                saveSearchFilters();
+
                 Intent swiperIntent = new Intent(SearchFilterActivity.this, SwiperActivity.class);
                 swiperIntent.putExtra("recipeAmount", 7);
                 startActivity(swiperIntent);
@@ -33,5 +63,9 @@ public class SearchFilterActivity extends BaseActivity {
                 finish();
             }
         });
+    }
+
+    public void saveSearchFilters() {
+        // Extract and save all the data that the user has selected and pass this data to the swiper activity?
     }
 }
